cc846e4e5225283da8365923d24683241da178a2
arttrh <arthurlucasx696@gmail.com>
2026-08-16 18:43:37 -0300

    Feat: Client DTO request

--- src/main/java/aeroporto_project/application/dto/Request/ClientRequestDTO.java
+++ src/main/java/aeroporto_project/application/dto/Request/ClientRequestDTO.java
@@ -1,6 +1,7 @@
 package aeroporto_project.application.dto.Request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 
 // criar o Pattern dos atributos
 public record ClientRequestDTO(
@@ -8,10 +9,12 @@ public record ClientRequestDTO(
   String nome,
   @NotBlank
   String email,
-  @NotBlank
-  String documento,
   @NotBlank 
-  String telefone
+  @Pattern(regexp = "\\d{2}\\.\\d{3}\\.\\d{3}-\\d{1}", message = "O Rg deve conter esse padrão: 99.999.999-9")
+  String rg,
+  @NotBlank
+  @Pattern(regexp = "\\d{3}\\.\\d{3}\\.\\d{3}-\\.\\d{2}", message = "O cpf deve conter esse padrão 000.000.000-00")
+  String cpf
 ) {
 }
 
